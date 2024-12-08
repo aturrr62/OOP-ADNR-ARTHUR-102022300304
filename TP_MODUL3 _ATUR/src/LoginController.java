de368a870d17a49d888d2d734f1f57e846a38763
aturrr62 <151583569+aturrr62@users.noreply.github.com>
2024-12-08 20:50:20 +0700

    Add files via upload

---
+++ TP_MODUL3 _ATUR/src/LoginController.java
@@ -0,0 +1,35 @@
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class LoginController {
+
+    @FXML
+    private TextField usernameField;
+
+    @FXML
+    private PasswordField passwordField;
+
+    @FXML
+    public void login(ActionEvent event) {
+        String username = usernameField.getText();
+        String password = passwordField.getText();
+
+        if (username.equals("vincent102022330236") && password.equals("si4706")) {
+            try {
+                App.changeScene("inventory.fxml");
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        } else {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Login Error");
+            alert.setHeaderText(null);
+            alert.setContentText("Username atau Password salah!");
+            alert.showAndWait();
+        }
+    }
+}
